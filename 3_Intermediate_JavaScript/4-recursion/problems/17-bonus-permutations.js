bca5fa776fad8bfe8b0b2fda6803d142e7e885fe
Anna Messejnikov <anna.messejnikov@gmail.com>
2022-10-04 21:53:14 -0500

    finished permutations

--- 3_Intermediate_JavaScript/4-recursion/problems/17-bonus-permutations.js
+++ 3_Intermediate_JavaScript/4-recursion/problems/17-bonus-permutations.js
@@ -12,7 +12,31 @@ permutations([1, 2, 3]) // [[1, 2, 3], [1, 3, 2],
                         // [3, 1, 2], [3, 2, 1]]
 ***********************************************************************/
 
-// your code here
+function permutations(array){
+  let permutation = [];
+
+  const dfs = (i, nums) => {
+    if(i === nums.length){
+      permutation.push(nums.slice());
+      return;
+    }
+
+    for(let j = i; j < nums.length; j++){
+      [nums[i], nums[j]] = [nums[j], nums[i]];
+      dfs(i + 1, nums);
+      [nums[i], nums[j]] = [nums[j], nums[i]];
+    }
+  }
+
+  dfs(0, array);
+  console.log(permutation);
+}
+
+//permutations([1, 2]); // [[1, 2], [2, 1]]
+permutations([1, 2, 3]); // [[1, 2, 3], [1, 3, 2],
+                        // [2, 1, 3], [2, 3, 1],
+                        // [3, 1, 2], [3, 2, 1]]
+
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS LINE*****************/
 try {
